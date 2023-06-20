3febfe079b3b798afbcedea9eb49ef0597709384
Fernando4077 <132718112+Fernando4077@users.noreply.github.com>
2023-06-19 23:26:54 -0600

    first commit

---
+++ scrip.js
@@ -0,0 +1,37 @@
+alert("Lista de Contactos");
+const arreglo = [
+    ["id", "0307-1999-00108"],
+    ["nombre","Alejandor"],
+    ["apellido","Gamez"],
+    ["ubicacion",
+        ["ciudad","tegucigalpa"],
+        ["direccion","granda"],
+    ]
+]
+//console.log(arreglo);
+
+
+//const eliminados = arreglo.splice(0);
+//console.log('Eliminado; ', eliminados);
+//console.log(arreglo);
+
+const añadir = arreglo.splice(1,0
+    [
+        ["id", "1611-1972-00900"],
+        ["nombre","Luz"],
+        ["apellido","Fernandez"],
+        ["ubicacion",
+            ["ciudad","Gualala"],
+            ["direccion","centro"],
+        ]
+    ]
+    );
+console.log('Agregados; ', añadir);
+//console.log(arreglo);
+
+
+
+
+
+
+
